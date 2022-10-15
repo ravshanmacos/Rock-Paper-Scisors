110fd7db12955585d9101d24cb2e10991ac92441
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2022-10-15 14:48:13 +0500

    Initial Commit

---
+++ Rock-Paper-Scisors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rock-Paper-Scisors
+//
+//  Created by Ravshanbek Tursunbaev on 2022/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
